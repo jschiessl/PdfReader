0606a3194bf33b73f5074f1dcd88fcef564cbda2
Jonas Schießl <jschiessl@munichre.com>
2021-08-22 08:30:18 +0200

    Add project files.

---
+++ PdfReader/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using BitMiracle.Docotic.Pdf;
+
+namespace PdfReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var pdf = new PdfDocument(Path.Combine(Directory.GetCurrentDirectory(), "test.pdf")))
+            {
+                var options = new PdfTextExtractionOptions
+                {
+                    SkipInvisibleText = true,
+                    WithFormatting = true
+                };
+                string formattedText = pdf.GetText(options);
+                Console.WriteLine(formattedText);
+            }
+        }
+    }
+}
